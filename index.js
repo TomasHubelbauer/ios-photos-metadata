ab51a106638ce268fc2e3dab25b23b0df8279091
Tomas Hubelbauer <tomas@hubelbauer.net>
2019-07-11 21:38:01 +0200

    Catch and display errors to see what's failing in Safari

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 window.addEventListener('load', () => {
-  const canvas = document.createElement('canvas');
-  canvas.width = 100;
-  canvas.height = 100;
-  document.body.append(canvas);
+  try {
+    const canvas = document.createElement('canvas');
+    canvas.width = 100;
+    canvas.height = 100;
+    document.body.append(canvas);
 
-  const context = canvas.getContext('2d');
-  context.fillRect(0, 0, 100, 100);
+    const context = canvas.getContext('2d');
+    context.fillRect(0, 0, 100, 100);
 
-  const downloadA = document.createElement('a');
-  downloadA.textContent = 'Download';
-  downloadA.download = 'backup.png';
-  downloadA.href = canvas.toDataURL();
+    const downloadA = document.createElement('a');
+    downloadA.textContent = 'Download';
+    downloadA.download = 'backup.png';
+    downloadA.href = canvas.toDataURL();
 
-  document.body.append(downloadA);
+    document.body.append(downloadA);
+  } catch (error) {
+    alert(error.messsage);
+  }
 });
