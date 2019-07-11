77d21159153d84fd621aba9d1831eb28aafa52b4
Tomas Hubelbauer <tomas@hubelbauer.net>
2019-07-11 21:35:15 +0200

    Try generated PNGs to append content to

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 window.addEventListener('load', () => {
+  const canvas = document.createElement('canvas');
+  canvas.width = 100;
+  canvas.height = 100;
+  document.body.append(canvas);
+
+  const context = canvas.getContext('2d');
+  context.fillRect(0, 0, 100, 100);
+
   const downloadA = document.createElement('a');
   downloadA.textContent = 'Download';
   downloadA.download = 'backup.png';
-  downloadA.href = `data:image/png,TEST`;
+  downloadA.href = canvas.toDataURL();
 
   document.body.append(downloadA);
 });
